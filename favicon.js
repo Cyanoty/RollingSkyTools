f5006a3e6742f2b65e2624f1037e60f46a848330
Cyanoty_ <151440240+Cyanoty@users.noreply.github.com>
2024-06-27 17:13:20 +0800

    6.27

--- favicon.js
+++
@@ -1,7 +0,0 @@
-var favNumber = Math.floor(Math.random() * 5)+1;
-var fav = document.createElement('link');
-fav.rel = 'icon';
-// fav.href = '../../assets/favicon/Ball'+favNumber+'.png';
-// fav.href = '../../assets/favicon/Icon'+favNumber+'.png'
-fav.href = "../../assets/favicon/ICON.png"
-document.getElementsByTagName('head')[0].appendChild(fav);
